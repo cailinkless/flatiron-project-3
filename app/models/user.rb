8d637deeed5dc13a66c4909b11943664fa9f67fd
cailinkless <cailin.nysff@gmail.com>
2020-12-04 11:43:03 -0500

    fix bug in github omniauth signin

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   # accepts_nested_attributes_for :productions
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid.first_or_create) do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.username = auth.info.name
       user.uid = auth.uid
